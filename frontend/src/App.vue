9c1df2bed7470a0cf2b6787841d83ff61a1c0126
delfin <carljefferson.delfin@gmail.com>
2025-08-07 16:16:35 +0800

    update route path

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { Toaster } from '@/components/ui/sonner'
+import { useRoute } from 'vue-router'
 import 'vue-sonner/style.css'
+
+const route = useRoute()
 </script>
 
 <template>
   <Toaster />
-  <router-view :key="$route.fullPath" />
+  <router-view :key="route.fullPath" />
 </template>
 
 <style scoped></style>
